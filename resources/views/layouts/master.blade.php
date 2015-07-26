10c7098b05754378a7a94fd8dcec156051b9a449
unknown <lexicon@app.com>
2015-07-26 06:46:44 -0700

    Before change layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
   <div class="container">
 
       <!-- Static navbar -->
-      <nav class="navbar navbar-default">
+      <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -53,7 +53,7 @@
       </nav>
 
      <main>
-    <div class="container">
+    <div class="container-fluid">
         @yield('content')
     </div>
 </main>
--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,30 +1,50 @@
 @extends('layouts.master')
-@include('layouts.center-modal')
+@section('css')
+  <style>
+   .modal-header, h4, .close {
+      background-color: #5cb85c;
+      color:white !important;
+      text-align: center;
+      font-size: 30px;
+  }
+  .modal-footer {
+      background-color: #f9f9f9;
+  }
+  .navbar-custom {
+    color: #FFFFFF;
+    background-color: #5cb85c;
+}
+  </style>
+@stop
+
 @section('content')
-<h1>Ticket List</h1>
+
+<fieldset> 
+  <legend> Ticket List</legend>   
 <ul>
     @foreach($tickets as $ticket)
 		<li> <a href="{{action('TicketController@show', ['id' => $ticket->tk_id])}}" 
 				  data-toggle="modal" data-target="#myModal">{{$ticket->tk_id}}</a>
     @endforeach
 </ul>
-    <div id="myModal" class="modal fade">
-        <div class="modal-dialog ">
+</fieldset> 
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <!-- Content will be loaded here from "remote.php" file -->
             </div>
         </div>
     </div>
 
- <div class="modal " id="myModal2" data-backdrop="static">
+ <div class="modal" id="myModal2" data-backdrop="static">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header bg-primary">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
           <h4 class="modal-title">Second Modal title</h4>
         </div><div class="container"></div>
         <div class="modal-body">
-          Content for the dialog / modal goes here.
+          <!--Content for the dialog / modal goes here.-->
         </div>
         <div class="modal-footer">
           <a href="#" data-dismiss="modal" class="btn">Close</a>
